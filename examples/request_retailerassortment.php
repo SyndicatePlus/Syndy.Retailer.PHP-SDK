ce5e2f53b078f301120ab9163972582e8390ad6a
Rogier <rogier@made.men>
2016-07-13 14:33:07 +0200

    Updated retailer assortment API example

--- examples/request_retailerassortment.php
+++ examples/request_retailerassortment.php
@@ -3,18 +3,20 @@
  * This Syndy Retailer Api example uses the higher level SyndyRetailerApiManager
  * class to create a retailer assortment request to fetch a retailer assortment.
  */
-define('APIKEY', 'Your Public Key Here');
-define('APISECRET', 'Your Private Key Here');
+define('PUBLICKEY', 'Your Public Key Here');
+define('PRIVATEKEY', 'Your Private Key Here');
 
 require_once "../src/syndyretailerapimanager.class.php";
 
 // Create a credentials object which holds the user's keys and access token
-$credentials = new Syndy\Api\Auth\SyndyApiCredentials(APIKEY, APISECRET);
+$credentials = new Syndy\Api\Auth\SyndyApiCredentials(PUBLICKEY, PRIVATEKEY);
 $api = new Syndy\Api\SyndyRetailerApiManager($credentials);
 
 // Create a request that fetches only that part of the assortment in which
 // changes have been made since yesterday.
-$request = $api->createRetailerAssortmentRequest(time() - 86400);
+$request = $api->createRetailerAssortmentRequest(time() - 4592000);
+// $request->setAmount(100);	// Set bigger page size
+// $request->setOffset(50);		// From what offset (0-based) to start fetching results
 $response = $request->execute();
 var_dump($response);
 
